dc7cffdc778a51b3f44b5f64bf50657dde7c3bb3
psbvision <punitsinghbisht1529@gmail.com>
2025-10-20 23:57:50 +0530

    feat: add responsiveness

--- ParallaxEffect/src/main.js
+++ ParallaxEffect/src/main.js
@@ -1,8 +1,16 @@
+const container = document.querySelector(".container");
+new ResizeObserver(() => {
+  document.documentElement.style.setProperty(
+    "--scale",
+    Math.min(container.parentElement.offsetWidth / container.offsetWidth, 1)
+  );
+});
+
 async function main() {
-  const canvas = document.getElementById('gameCanvas');
-  const c = canvas.getContext('2d');
+  const canvas = document.getElementById("gameCanvas");
+  const c = canvas.getContext("2d");
   c.imageSmoothingEnabled = false;
-  c.fillStyle = 'black';
+  c.fillStyle = "black";
   c.fillRect(0, 0, canvas.width, canvas.height);
 }
 
