7e48592890916424e30fa3bd1c5572612b81b213
psbvision <punitsinghbisht1529@gmail.com>
2025-10-22 18:17:12 +0530

    feat: complete infinite parallax Effect

--- ParallaxEffect/src/main.js
+++ ParallaxEffect/src/main.js
@@ -1,10 +1,13 @@
-import { loadSprite } from "./utils";
+import { loadSprite, makeInfiniteScroll, makeLayer, makeSprite } from "./utils.js";
 
 const container = document.querySelector(".container");
 new ResizeObserver(() => {
   document.documentElement.style.setProperty(
     "--scale",
-    Math.min(container.parentElement.offsetWidth / container.offsetWidth, container.parentElement.offsetHeight / container.offsetHeight)
+    Math.min(
+      container.parentElement.offsetWidth / container.offsetWidth,
+      container.parentElement.offsetHeight / container.offsetHeight
+    )
   );
 }).observe(container.parentElement);
 
@@ -15,14 +18,39 @@ async function main() {
   c.fillStyle = "black";
   c.fillRect(0, 0, canvas.width, canvas.height);
 
-  const [layer1, layer2, layer3, layer4] =await Promise.all([
+  const [layer1, layer2, layer3, layer4] = await Promise.all([
     loadSprite("./assets/1.png"),
     loadSprite("./assets/2.png"),
     loadSprite("./assets/3.png"),
     loadSprite("./assets/4.png"),
   ]);
 
-  
+  const layer1GameObj = makeSprite(c, layer1, { x: 0, y: -100 }, 4);
+  const layer2GameObj = makeLayer(c, layer2, { x: 0, y: -100 }, 4);
+  const layer3GameObj = makeLayer(c, layer3, { x: 0, y: -100 }, 4);
+  const layer4GameObj = makeLayer(c, layer4, { x: 0, y: -100 }, 4);
+
+  let dt;
+  let oldTimeStamp = 0;
+  let fps;
+  const debugMode = false;
+  function gameLoop(timestamp) {
+    dt = (timestamp - oldTimeStamp) / 1000;
+    oldTimeStamp = timestamp;
+    fps = Math.round(1 / dt);
+    c.clearRect(0, 0, canvas.width, canvas.height);
+    layer1GameObj.draw();
+    makeInfiniteScroll(dt, layer2GameObj, -100);
+    makeInfiniteScroll(dt, layer3GameObj, -700);
+    makeInfiniteScroll(dt, layer4GameObj, -1200);
+    if (debugMode){
+      c.font = "128px Arial";
+      c.fillStyle = "black";
+      c.fillText(fps, 25, 120);
+    }
+    requestAnimationFrame(gameLoop);
+  }
+  requestAnimationFrame(gameLoop);
 }
 
 main();
--- ParallaxEffect/src/utils.js
+++ ParallaxEffect/src/utils.js
@@ -1,8 +1,51 @@
-export function loadSprite(src){
+export function loadSprite(src) {
   return new Promise((resolve, reject) => {
     const img = new Image();
-    img.load = () => resolve(img);
+    img.onload = () => resolve(img);
     img.onerror = (err) => reject(err);
-    img.src = src;
+    img.src = src + "?t=" + new Date().getTime();
   });
+}
+
+export function makeSprite(c, sprite, pos, scale = 1) {
+  return {
+    width: sprite.width,
+    height: sprite.height,
+    pos,
+    scale,
+    draw() {
+      c.drawImage(
+        sprite,
+        this.pos.x,
+        this.pos.y,
+        this.width * scale,
+        this.height * scale
+      );
+    },
+  };
+}
+
+export function makeLayer(c, sprite, pos, scale = 1) {
+  return {
+    head: makeSprite(c, sprite, pos, scale),
+    tail: makeSprite(
+      c,
+      sprite,
+      { x: pos.x + sprite.width * scale, y: pos.y },
+      scale
+    ),
+  };
+}
+
+export function makeInfiniteScroll(dt, layer, speed) {
+  if (layer.head.pos.x + layer.head.width * layer.head.scale < 0) {
+    layer.head.pos.x = layer.tail.pos.x + layer.tail.width * layer.tail.scale;
+  }
+  if (layer.tail.pos.x + layer.tail.width * layer.head.scale < 0) {
+    layer.tail.pos.x = layer.head.pos.x + layer.head.width * layer.tail.scale;
+  }
+  layer.head.pos.x += speed * dt;
+  layer.head.draw();
+  layer.tail.pos.x += speed * dt;
+  layer.tail.draw();
 }
